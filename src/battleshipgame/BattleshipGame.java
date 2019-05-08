3267d789622082a272441b3426527576409954df
zaadie <saadiyafourie@gmail.com>
2019-05-08 14:05:05 +1000

    Grid

--- src/battleshipgame/BattleshipGame.java
+++ src/battleshipgame/BattleshipGame.java
@@ -1,6 +1,9 @@
 package battleshipgame;
 
 import battleshipmodel.*;
+import java.util.Scanner;
+import java.util.Arrays;
+
 
 // The BattleshipGame class contains the driver implementation
 // for the Battleship game.
@@ -12,6 +15,13 @@ public class BattleshipGame {
         System.out.println("Welcome to the Battleship game.");
         
         // Your implementation goes here.
-        
+        String[][] board = new String [10][10];
+        for (int r = 0; r<board.length;r++){
+        	   for (int c = 0; c <board.length;c++){
+        	      board[r][c] = " ";  // Initialize the cell
+        	      System.out.print("[" +board[r][c] + "]"); // Display the content of cell board
+        	   }
+        	   System.out.println();  // go to next line
+        }
     }
 }
